2fc4806ab606b3e83b7c49906e674ea5216cc3e7
Jorge <desarrollador117_usi@minedu.gob.pe>
2024-05-09 08:48:50 -0500

    se modifican los environments

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  // apiURL2: 'https://fakestoreapi.com',
-  apiURL: 'http://localhost:4242/api',
-  stripeAPIKey: 'pk_test_51Ig8ZUITaHEt1i2yGUb7JViKcGXQg8oHVT8Ax2NJfJwKzrn6t9Vw2fW4npFmZYqxbrSJ5o6jCc7JnpeI1ZyY09fD00O7fimZar',
-  serverURL: 'http://localhost:4242',
-  SUPABASE_URL: 'https://blijhwisxhocmojszgoy.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsaWpod2lzeGhvY21vanN6Z295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0NTIwNzIsImV4cCI6MjAzMDAyODA3Mn0.yLvFw7dL4wHD33XlYqZ7Ko32bDMCQtJMew_wNeRwDUw',
-  SUPABASE_SERVICE_ROLE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsaWpod2lzeGhvY21vanN6Z295Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNDQ1MjA3MiwiZXhwIjoyMDMwMDI4MDcyfQ.vZzAONDWyZxhRjknotmirmRJVlb0D4J4YYOTuOTFLw8',
-};
