7cdb49090b01cffd8f369a1dc77bc43cfa6dfcd9
Jorge <desarrollador117_usi@minedu.gob.pe>
2024-06-04 13:00:52 -0500

    configurando integracion continua y variable de entorno prod

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  
+  apiURL: '',
+  SUPABASE_URL: '',
+  SUPABASE_ANON_KEY: '',
+  SUPABASE_SERVICE_ROLE_KEY: '',
+  RECAPTCHA_V3_STACKBLITZ_KEY: '',
+  RECAPTCHA_V2_DUMMY_KEY: '',
+};
