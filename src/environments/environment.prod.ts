0ebadca1005fc9cad5e0a089ceecf2dec5254873
Jorge <desarrollador117_usi@minedu.gob.pe>
2024-06-05 12:05:13 -0500

    configuracion de docker y variable de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
+// environment.ts
+// export interface Environment {
+//   API_URL: string;
+// }
+
+// export const environment = {
+//   production: true,
+//   // apiUrl: 'http://165.232.144.187:8081'
+//   apiURL: '${API_URL}',
+//   SUPABASE_URL: '',
+//   SUPABASE_ANON_KEY: '',
+//   SUPABASE_SERVICE_ROLE_KEY: '',
+//   RECAPTCHA_V3_STACKBLITZ_KEY: '',
+//   RECAPTCHA_V2_DUMMY_KEY: '',
+// }
+
+
+
 export const environment = {
   production: true,
   // environment: $ENV.ENVIRONMENT,
@@ -15,16 +33,20 @@ export const environment = {
 
 };
 
-// export const environment = {
-//   production: true,
-//   // apiURL2: 'https://fakestoreapi.com',
-//   // apiURL: 'http://localhost:4242/api',
-//   apiURL: 'http://happychecho.qhatuyki.shop/api',
-//   // stripeAPIKey: 'pk_test_51Ig8ZUITaHEt1i2yGUb7JViKcGXQg8oHVT8Ax2NJfJwKzrn6t9Vw2fW4npFmZYqxbrSJ5o6jCc7JnpeI1ZyY09fD00O7fimZar',
-//   // serverURL: 'http://localhost:4242',
-//   SUPABASE_URL: 'https://blijhwisxhocmojszgoy.supabase.co',
-//   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsaWpod2lzeGhvY21vanN6Z295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0NTIwNzIsImV4cCI6MjAzMDAyODA3Mn0.yLvFw7dL4wHD33XlYqZ7Ko32bDMCQtJMew_wNeRwDUw',
-//   SUPABASE_SERVICE_ROLE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsaWpod2lzeGhvY21vanN6Z295Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNDQ1MjA3MiwiZXhwIjoyMDMwMDI4MDcyfQ.vZzAONDWyZxhRjknotmirmRJVlb0D4J4YYOTuOTFLw8',
-//   RECAPTCHA_V3_STACKBLITZ_KEY: '6Ldl4dgpAAAAAOaitS_6xpc0-5aQcOeCoOvTkpNi',
-//   RECAPTCHA_V2_DUMMY_KEY: '6Lcbt1waAAAAAFiUe0tUSxbjivzoQe_boiXfFRf_',
-// };
+
+
+
+
+// // export const environment = {
+// //   production: true,
+// //   // apiURL2: 'https://fakestoreapi.com',
+// //   // apiURL: 'http://localhost:4242/api',
+// //   apiURL: 'http://happychecho.qhatuyki.shop/api',
+// //   // stripeAPIKey: 'pk_test_51Ig8ZUITaHEt1i2yGUb7JViKcGXQg8oHVT8Ax2NJfJwKzrn6t9Vw2fW4npFmZYqxbrSJ5o6jCc7JnpeI1ZyY09fD00O7fimZar',
+// //   // serverURL: 'http://localhost:4242',
+// //   SUPABASE_URL: 'https://blijhwisxhocmojszgoy.supabase.co',
+// //   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsaWpod2lzeGhvY21vanN6Z295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0NTIwNzIsImV4cCI6MjAzMDAyODA3Mn0.yLvFw7dL4wHD33XlYqZ7Ko32bDMCQtJMew_wNeRwDUw',
+// //   SUPABASE_SERVICE_ROLE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsaWpod2lzeGhvY21vanN6Z295Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNDQ1MjA3MiwiZXhwIjoyMDMwMDI4MDcyfQ.vZzAONDWyZxhRjknotmirmRJVlb0D4J4YYOTuOTFLw8',
+// //   RECAPTCHA_V3_STACKBLITZ_KEY: '6Ldl4dgpAAAAAOaitS_6xpc0-5aQcOeCoOvTkpNi',
+// //   RECAPTCHA_V2_DUMMY_KEY: '6Lcbt1waAAAAAFiUe0tUSxbjivzoQe_boiXfFRf_',
+// // };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,18 @@
+// export interface Environment {
+//   API_URL: string;
+// }
+
+// export const environment = {
+//   production: false,
+//   // apiUrl: 'http://165.232.144.187:8081'
+//   apiURL: '${API_URL}',
+//   SUPABASE_URL: '',
+//   SUPABASE_ANON_KEY: '',
+//   SUPABASE_SERVICE_ROLE_KEY: '',
+//   RECAPTCHA_V3_STACKBLITZ_KEY: '',
+//   RECAPTCHA_V2_DUMMY_KEY: '',
+// }
+
 export const environment = {
   production: false,
   
