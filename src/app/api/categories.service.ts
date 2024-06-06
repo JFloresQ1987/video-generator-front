cd091a7b03d983b100918a3d279508927f28e5f4
Jorge <desarrollador117_usi@minedu.gob.pe>
2024-06-05 19:12:09 -0500

    configuracion de docker y variable de entorno

--- src/app/api/categories.service.ts
+++ src/app/api/categories.service.ts
@@ -7,9 +7,9 @@ import {
 import { environment } from '@envs/environment';
 import { Category } from '@shared/models/category.interface';
 import { map, tap } from 'rxjs';
-import Config from '@envs/config.json'
+// import Config from '@envs/config.json'
 // import Config from '../../assets/config.json'
-// import Config from '../../config.json'
+import Config from '../../config.json'
 
 @Injectable({ providedIn: 'root' })
 export class CategoriesService {
